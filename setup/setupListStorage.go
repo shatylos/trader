f729ebfb04d9e3fab52f980ecd025fd24d7fe8b5
Serhii Shatylo <serhii@shatylo.pp.ua>
2022-08-28 19:58:30 +0300

    change setup config

--- setup/setupListStorage.go
+++ setup/setupListStorage.go
@@ -26,7 +26,7 @@ func setupListInit() {
 			TimeoutSeconds:   10,
 			AvgCostShift:     30,
 			Leverage:         1,
-			Qty:              0.02,
+			Qty:              0.05,
 			TakeProfitSize:   150,
 			StopLossSize:     450,
 		},
