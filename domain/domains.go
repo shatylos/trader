b475ce78337622ca9de9297599ecfcda7fea2e03
Serhii Shatylo <serhii@shatylo.pp.ua>
2022-10-01 21:13:59 +0300

    Added bybit prod domain

--- domain/domains.go
+++ domain/domains.go
@@ -17,10 +17,12 @@ var (
 func init() {
 	domainList = append(domainList, constant.DomainExmo)
 	domainList = append(domainList, constant.DomainExmoMargin)
+	domainList = append(domainList, constant.DomainBybitMargin)
 	domainList = append(domainList, constant.DomainBybitMarginDemo)
 
 	domainInterfaceList[constant.DomainExmo] = &exmo.DomainExmo{}
 	domainInterfaceList[constant.DomainExmoMargin] = &exmo.DomainExmoMargin{}
+	domainInterfaceList[constant.DomainBybitMargin] = &bybit.DomainBybitMargin{IsDemo: false}
 	domainInterfaceList[constant.DomainBybitMarginDemo] = &bybit.DomainBybitMargin{IsDemo: true}
 }
 
