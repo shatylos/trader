41f5fff31e9316f4d7beeb4ce81a757302ec528d
Serhii Shatylo <serhii@shatylo.pp.ua>
2025-02-04 22:26:31 +0200

    Memory leak fixes

--- utils/math.go
+++ utils/math.go
@@ -5,13 +5,15 @@ import "github.com/shopspring/decimal"
 func Mul(a, b float64) float64 {
 	num1 := decimal.NewFromFloat(a)
 	num2 := decimal.NewFromFloat(b)
-	result, _ := num1.Mul(num2).Float64()
-	return result
+	result := num1.Mul(num2)
+	resultFloat, _ := result.Float64()
+	return resultFloat
 }
 
 func Div(a, b float64) float64 {
 	num1 := decimal.NewFromFloat(a)
 	num2 := decimal.NewFromFloat(b)
-	result, _ := num1.Div(num2).Float64()
-	return result
+	result := num1.Div(num2)
+	resultFloat, _ := result.Float64()
+	return resultFloat
 }
