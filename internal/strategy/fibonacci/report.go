3f3c8cbf7747c86a750462880a7b81b83e040a45
Serhii Shatylo <serhii@shatylo.pp.ua>
2025-04-14 23:04:23 +0300

    Added balance and pnl amounts to report page

--- internal/strategy/fibonacci/report.go
+++ internal/strategy/fibonacci/report.go
@@ -6,6 +6,7 @@ import (
 	"github.com/shatylos/trader/internal/strategy/fibonacci/storage/mongo"
 	"github.com/shatylos/trader/internal/strategy/fibonacci/structs"
 	_struct "github.com/shatylos/trader/internal/strategy/struct"
+	"github.com/shatylos/trader/tools/math"
 	"github.com/shatylos/trader/web/helper"
 	"html/template"
 	"strings"
@@ -16,14 +17,18 @@ type ReportOrderItem struct {
 }
 
 type TemplateData struct {
-	PrevPeriodLink string
-	NextPeriodLink string
-	DateFrom       time.Time
-	DateTo         time.Time
-	Positions      []structs.Position
-	MainCurrency   string
-	TradeCurrency  string
-	PricePrecision int
+	PrevPeriodLink  string
+	NextPeriodLink  string
+	DateFrom        time.Time
+	DateTo          time.Time
+	Positions       []structs.Position
+	MainCurrency    string
+	TradeCurrency   string
+	PricePrecision  int
+	TotalPnl        float64
+	TotalPnlPercent float64
+	BalanceBefore   float64
+	BalanceAfter    float64
 }
 
 func (f *Fibonacci) GetReport(from time.Time, to time.Time) (report _struct.Report, err error) {
@@ -39,16 +44,21 @@ func (f *Fibonacci) GetReport(from time.Time, to time.Time) (report _struct.Repo
 	}
 	var positions []structs.Position
 	positions, err = storage.GetPositions(from, to)
+	totalPnl, totalPnlPercent, balanceBefore, balanceAfter := f.reportAmounts(positions)
 
 	data := TemplateData{
-		PrevPeriodLink: fmt.Sprintf("/report/%s/%s/", f.GetId(), from.AddDate(0, 0, -1).Format("2006-01")),
-		NextPeriodLink: fmt.Sprintf("/report/%s/%s/", f.GetId(), from.AddDate(0, 1, 0).Format("2006-01")),
-		DateFrom:       from,
-		DateTo:         to,
-		Positions:      positions,
-		MainCurrency:   f.config.MainCurrency,
-		TradeCurrency:  f.config.TradeCurrency,
-		PricePrecision: int(f.config.PricePrecision),
+		PrevPeriodLink:  fmt.Sprintf("/report/%s/%s/", f.GetId(), from.AddDate(0, 0, -1).Format("2006-01")),
+		NextPeriodLink:  fmt.Sprintf("/report/%s/%s/", f.GetId(), from.AddDate(0, 1, 0).Format("2006-01")),
+		DateFrom:        from,
+		DateTo:          to,
+		Positions:       positions,
+		MainCurrency:    f.config.MainCurrency,
+		TradeCurrency:   f.config.TradeCurrency,
+		PricePrecision:  int(f.config.PricePrecision),
+		TotalPnl:        totalPnl,
+		TotalPnlPercent: totalPnlPercent,
+		BalanceBefore:   balanceBefore,
+		BalanceAfter:    balanceAfter,
 	}
 
 	var resultBuilder strings.Builder
@@ -62,6 +72,26 @@ func (f *Fibonacci) GetReport(from time.Time, to time.Time) (report _struct.Repo
 	report = _struct.Report{
 		InnerHtml: template.HTML(htmlStr),
 		SetupId:   f.GetId(),
+	}
+	return
+}
+
+func (f *Fibonacci) reportAmounts(positions []structs.Position) (totalPnl float64, totalPnlPercent float64, balanceBefore float64, balanceAfter float64) {
+	filteredPositions := []structs.Position{}
+	for _, position := range positions {
+		if position.Status == structs.StatusClosed {
+			filteredPositions = append(filteredPositions, position)
+			totalPnl += position.TotalClosePnl
+		}
+	}
+	if len(filteredPositions) > 0 {
+		balanceBefore = filteredPositions[len(filteredPositions)-1].BalanceBefore
+		balanceAfter = filteredPositions[0].BalanceAfter
+	}
+	if totalPnl > 0 && balanceBefore > 0 && balanceAfter > 0 {
+		onePercent := math.Div(balanceBefore, 100)
+		amountDiff := balanceAfter - balanceBefore
+		totalPnlPercent = math.Div(amountDiff, onePercent)
 	}
 	return
 }
