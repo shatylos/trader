dad15e6756959c18e6c616fd60a1ef36eae498d4
Serhii Shatylo <serhii@shatylo.pp.ua>
2025-04-21 09:49:14 +0300

    Fixed error unexpected trend

--- internal/strategy/fibonacci/positionAction.go
+++ internal/strategy/fibonacci/positionAction.go
@@ -18,18 +18,12 @@ func (f *Fibonacci) actionByPosition(internalPosition structs.Position, currentP
 		err = f.actionByPositionBearish(internalPosition, currentPrice)
 		break
 	default:
-		err = tools.AppError{
-			Message: fmt.Sprintf("Unexpected trend %s for action by position", internalPosition.Trend),
-		}
+		logger.Info(fmt.Sprintf("Trend is %s. Wait for %s or %s.", internalPosition.Trend, trading.TrendLong, trading.TrendShort))
 	}
 	return
 }
 
 func (f *Fibonacci) actionByPositionBullish(internalPosition structs.Position, currentPrice float64) (err error) {
-
-	logger.Info(fmt.Sprintf("internalPosition.Status %s", internalPosition.Status))
-	logger.Info(fmt.Sprintf("internalPosition.BalanceBefore %g", internalPosition.BalanceBefore))
-
 	if internalPosition.Status == structs.StatusNew && internalPosition.BalanceBefore <= 0 {
 		msg := fmt.Sprintf("Not enough balance (%g) to action", internalPosition.BalanceBefore)
 		logger.Error(msg)
@@ -125,10 +119,6 @@ func (f *Fibonacci) actionByPositionBullish(internalPosition structs.Position, c
 }
 
 func (f *Fibonacci) actionByPositionBearish(internalPosition structs.Position, currentPrice float64) (err error) {
-
-	logger.Info(fmt.Sprintf("internalPosition.Status %s", internalPosition.Status))
-	logger.Info(fmt.Sprintf("internalPosition.BalanceBefore %g", internalPosition.BalanceBefore))
-
 	if internalPosition.Status == structs.StatusNew && internalPosition.BalanceBefore <= 0 {
 		msg := fmt.Sprintf("Not enough balance (%g) to action", internalPosition.BalanceBefore)
 		logger.Error(msg)
