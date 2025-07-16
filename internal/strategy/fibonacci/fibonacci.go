f850927b2d799fa84ff43debc333b0c204c2b87b
Serhii Shatylo <serhii@shatylo.pp.ua>
2025-07-16 21:01:34 +0300

    Fixed text label

--- internal/strategy/fibonacci/fibonacci.go
+++ internal/strategy/fibonacci/fibonacci.go
@@ -122,14 +122,14 @@ func (f *Fibonacci) newOrderCondition(internalPosition structs.Position) (condit
 			internalPosition.FibonacciChart.EntryPoint1 > 0.0 &&
 			internalPosition.FibonacciChart.EntryPoint2 > 0.0 {
 
-			condition = fmt.Sprintf("> %g, < %g", internalPosition.FibonacciChart.EntryPoint1, internalPosition.FibonacciChart.EntryPoint2)
+			condition = fmt.Sprintf("> %g and < %g", internalPosition.FibonacciChart.EntryPoint1, internalPosition.FibonacciChart.EntryPoint2)
 			return
 		}
 		if internalPosition.Orders.Order2.OrderId == "" &&
 			internalPosition.FibonacciChart.EntryPoint2 > 0.0 &&
 			internalPosition.FibonacciChart.EntryPoint3 > 0.0 {
 
-			condition = fmt.Sprintf("> %g, < %g", internalPosition.FibonacciChart.EntryPoint2, internalPosition.FibonacciChart.EntryPoint3)
+			condition = fmt.Sprintf("> %g and < %g", internalPosition.FibonacciChart.EntryPoint2, internalPosition.FibonacciChart.EntryPoint3)
 			return
 		}
 		if internalPosition.Orders.Order3.OrderId == "" &&
@@ -144,14 +144,14 @@ func (f *Fibonacci) newOrderCondition(internalPosition structs.Position) (condit
 			internalPosition.FibonacciChart.EntryPoint1 > 0.0 &&
 			internalPosition.FibonacciChart.EntryPoint2 > 0.0 {
 
-			condition = fmt.Sprintf("< %g, > %g", internalPosition.FibonacciChart.EntryPoint1, internalPosition.FibonacciChart.EntryPoint2)
+			condition = fmt.Sprintf("< %g and > %g", internalPosition.FibonacciChart.EntryPoint1, internalPosition.FibonacciChart.EntryPoint2)
 			return
 		}
 		if internalPosition.Orders.Order2.OrderId == "" &&
 			internalPosition.FibonacciChart.EntryPoint2 > 0.0 &&
 			internalPosition.FibonacciChart.EntryPoint3 > 0.0 {
 
-			condition = fmt.Sprintf("< %g, > %g", internalPosition.FibonacciChart.EntryPoint2, internalPosition.FibonacciChart.EntryPoint3)
+			condition = fmt.Sprintf("< %g and > %g", internalPosition.FibonacciChart.EntryPoint2, internalPosition.FibonacciChart.EntryPoint3)
 			return
 		}
 		if internalPosition.Orders.Order3.OrderId == "" &&
