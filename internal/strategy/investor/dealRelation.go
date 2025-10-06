d3f44376f8346c7f3d6219232f48aee6debc6873
Serhii Shatylo <serhii@shatylo.pp.ua>
2025-10-06 11:52:30 +0300

    INV: Deal relation total revenue (debug)

--- internal/strategy/investor/dealRelation.go
+++ internal/strategy/investor/dealRelation.go
@@ -91,7 +91,8 @@ func (i *Investor) GetDealRelation(ctx context.Context, deal *storage.Deal) (dea
 
 		//revenueTotal += mainAmountAfter - mainAmountBefore
 		//revenueTotal += tradeCurrencyToMain(tradeAmountAfter-tradeAmountBefore, order.Price)
-		revenueTotal += tradeCurrencyToMain(tradeAmountAfter, order.Price) - tradeCurrencyToMain(tradeAmountBefore, order.Price)
+		revenueTotal += tradeCurrencyToMain(tradeAmountAfter, order.Price)
+		revenueTotal -= tradeCurrencyToMain(tradeAmountBefore, order.Price)
 		//revenueTotal += tradeAmountAfter - tradeAmountBefore
 	}
 
