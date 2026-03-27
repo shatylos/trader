8ccf29d9377aeecfcb73413a5b4e231f2a5b0f02
Serhii Shatylo <serhii@shatylo.pp.ua>
2026-03-27 12:27:35 +0200

    INV: Made state message more clear

--- internal/strategy/investor/premiumDiscountHandling.go
+++ internal/strategy/investor/premiumDiscountHandling.go
@@ -130,9 +130,10 @@ func (i *Investor) handleDiscount(ctx context.Context, dealRelation *entity.Deal
 
 		currentPrice := timeFrameItem.Candles[0].Close
 		minAmountRange := math.Mul(math.Div(currentPrice, 100), timeFrameItem.Config.MinPercentRangeToBuyMore)
+		nextBuyPrice := minOrderPrice - minAmountRange
 		currentPriceRange := minOrderPrice - currentPrice
 		if currentPriceRange < minAmountRange {
-			timeFrameItem.TradeStateMsg = fmt.Sprintf("Expect lower price (%.2f) to handle discount", minOrderPrice)
+			timeFrameItem.TradeStateMsg = fmt.Sprintf("Expect lower price (%.2f) to handle discount", nextBuyPrice)
 			return
 		}
 		if !timeFrameItem.Config.CanOpenNewOrder {
